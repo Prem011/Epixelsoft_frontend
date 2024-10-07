fa75ba3e8193775bcdf7df3871b786dc89edb0a6
Prem <pwanjari761@gmail.com>
2024-10-07 14:37:37 +0530

    form validations

--- src/components/Page5/Page5.jsx
+++ src/components/Page5/Page5.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 
 const Page5 = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+
+  const onSubmit = async (data) => {
+    console.log("Form data:", data); // Log form data to console
+    reset(); // Optional: Resets form fields after submission
+    toast.success("Thankyou for Signing up!");
+  };
+
   return (
     <>
       <div className="mx-[4%] h-[90%]">
@@ -16,42 +34,64 @@ const Page5 = () => {
 
         <div className="h-[55%] flex justify-center">
           <div className="w-full md:w-[50%] lg:w-[40%] mt-[5%] border border-black px-[5%] py-[3%]">
-            <form className="flex flex-col">
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
               <h1 className="text-[6vw] md:text-2xl font-semibold mb-[5%]">
                 Sign up for free account
               </h1>
 
-              <div className="flex flex-col md:flex-row gap-[7%]">
-                <input
-                  type="text"
-                  placeholder="First Name"
-                  className="border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
-                />
-                <input
-                  type="text"
-                  placeholder="Last Name"
-                  className="border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
-                />
+              <div className="flex justify-between flex-col md:flex-row  gap-[7%]">
+                <label className="w-[45%] flex flex-col md:flex-row gap-[7%]">
+                  <input
+                    type="text"
+                    className="grow  border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
+                    placeholder="First Name"
+                    {...register("firstName", { required: true })}
+                  />
+                </label>
+                {errors.firstName && <span className="text-red-500 text-sm">First name is required</span>}
+
+                <label className="w-[45%] flex flex-col md:flex-row gap-[7%]">
+                  <input
+                    type="text"
+                    className="grow  border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
+                    placeholder="Last Name"
+                    {...register("lastName", { required: true })}
+                  />
+                </label>
+                {errors.lastName && <span className="text-red-500 text-sm">Last name is required</span>}
               </div>
 
-              <div className="flex flex-col md:flex-row gap-[7%] mt-[4%]">
-                <input
-                  type="text"
-                  placeholder="Email address"
-                  className="border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
-                />
-                <input
-                  type="text"
-                  placeholder="Phone Number"
-                  className="border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
-                />
+              <div className="justify-between flex flex-col md:flex-row gap-[7%] mt-[4%]">
+                <label className="w-[45%] flex flex-col md:flex-row gap-[7%]">
+                  <input
+                    type="text"
+                    className="grow  border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
+                    placeholder="Email address"
+                    {...register("email", { required: true })}
+                  />
+                </label>
+                {errors.email && <span className="text-red-500 text-sm">Email is required</span>}
+
+                <label className="w-[45%] flex flex-col md:flex-row gap-[7%]">
+                  <input
+                    type="text"
+                    className="grow  border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[2%]"
+                    placeholder="Phone Number"
+                    {...register("phoneNumber", { required: true })}
+                  />
+                </label>
+                {errors.phoneNumber && <span className="text-red-500 text-sm">Phone number is required</span>}
               </div>
 
-              <input
-                type="text"
-                placeholder="Phone Number"
-                className="mt-[6%] border-2 border-zinc-400 p-2 w-full"
-              />
+              <label className="flex flex-col md:flex-row gap-[7%]">
+                <input
+                  type="password"
+                  className="grow  border-2 border-zinc-400 p-2 w-full md:w-[50%] mt-[5%]"
+                  placeholder="Create Password"
+                  {...register("password", { required: true })}
+                />
+              </label>
+              {errors.password && <span className="text-red-500 text-sm">Password is required</span>}
 
               <button className="w-full md:w-[30%] mt-[6%] px-4 py-2 text-white bg-blue-400 border border-blue-400 rounded-md hover:text-blue-400 hover:bg-white transition">
                 Register
